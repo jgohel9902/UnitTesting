fec7741e43e32bdb1d3132e8ec2c564642fa069e
Jenil Gohel <jgohel9902@gmail.com>
2025-03-27 18:29:07 -0400

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            if (width <= 0 || height <= 0)
+            {
+                throw new ArgumentException("Width and Height must be positive values.");
+            }
+
+            Width = width;
+            Height = height;
+        }
+        public double GetArea()
+        {
+            return Width * Height;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * (Width + Height);
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Xunit;
+using Assignment2;
+
+public class RectangleTests
+{
+    // Test 1 : Constructor should initialize with valid values
+    [Fact]
+    public void Constructor_ShouldInitialize_WithValidValues()
+    {
+        var rectangle = new Rectangle(10, 5);
+        Assert.Equal(10, rectangle.Width);
+        Assert.Equal(5, rectangle.Height);
+    }
+
+    // Test 2 : Constructor should throw exception for negative values
+    [Fact]
+    public void Constructor_ShouldThrowException_WhenNegativeValuesProvided()
+    {
+        Assert.Throws<ArgumentException>(() => new Rectangle(-5, 10));
+    }
+
+    // Test 3 : GetArea should return correct area
+    [Fact]
+    public void GetArea_ShouldReturn_CorrectArea()
+    {
+        var rectangle = new Rectangle(10, 5);
+        double area = rectangle.GetArea();
+        Assert.Equal(50, area);
+    }
+
+    // Test 4 : GetPerimeter should return correct perimeter
+    [Fact]
+    public void GetPerimeter_ShouldReturn_CorrectPerimeter()
+    {
+        var rectangle = new Rectangle(10, 5);
+        double perimeter = rectangle.GetPerimeter();
+        Assert.Equal(30, perimeter);
+    }
+
+    // Test 5: Setting width should update correctly
+    [Fact]
+    public void SetWidth_ShouldUpdate_WidthValue()
+    {
+        var rectangle = new Rectangle(10, 5);
+        rectangle.Width = 15;
+        Assert.Equal(15, rectangle.Width);
+    }
+
+    // Test 6: Setting height should update correctly
+    [Fact]
+    public void SetHeight_ShouldUpdate_HeightValue()
+    {
+        var rectangle = new Rectangle(10, 5);
+        rectangle.Height = 20;
+        Assert.Equal(20, rectangle.Height);
+    }
+
+    //  Test 7: Zero width should throw exception
+    [Fact]
+    public void Constructor_ShouldThrowException_WhenZeroWidthProvided()
+    {
+        // Arrange & Act & Assert
+        Assert.Throws<ArgumentException>(() => new Rectangle(0, 10));
+    }
+
+    //  Test 8: Zero height should throw exception
+    [Fact]
+    public void Constructor_ShouldThrowException_WhenZeroHeightProvided()
+    { 
+        Assert.Throws<ArgumentException>(() => new Rectangle(10, 0));
+    }
+}
